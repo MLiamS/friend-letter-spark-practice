0ed73a77634ec0b77d6cd39f8c0b467a95e2bdce
liam stabeno and shane stafford <liam@>
2017-03-20 10:23:07 -0700

    complete spark practice page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,7 +5,6 @@ import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class App {
-
   public static void main(String[] args) {
 
     staticFileLocation("/public");
@@ -23,6 +22,21 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
-  }
+    get("/form", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
 
+    get("/greeting_card", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      String recipient = request.queryParams("recipient");
+      String sender = request.queryParams("sender");
+      model.put("recipient", recipient);
+      model.put("sender", sender);
+      model.put("template", "templates/greeting_card.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+  }
 }
